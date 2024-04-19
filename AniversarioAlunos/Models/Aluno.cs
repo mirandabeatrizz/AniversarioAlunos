41b5b26f31822732088edad0db6398031d2345eb
Beatriz Miranda <115667894+mirandabeatrizz@users.noreply.github.com>
2024-04-19 17:33:10 -0300

    ajustes para cadastrar e editar

--- AniversarioAlunos/Models/Aluno.cs
+++ AniversarioAlunos/Models/Aluno.cs
@@ -1,9 +1,18 @@
-﻿namespace AniversarioAlunos.Models
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace AniversarioAlunos.Models
 {
     public class Aluno
     {
         public int Id { get; set; }
+
+        [Required(ErrorMessage ="É preciso informar um nome.")]
+        [MinLength(3, ErrorMessage = "O nome deve conter pelo menos 3 caracteres.")]
         public string ?Nome { get; set; }
+
+        [Required(ErrorMessage ="é preciso informar a data de aniversário ")]
+      //  [YearRange(1900, 2024)]
         public DateOnly DataAniversario { get; set; }
 
         public Aluno() { }
